71320722769de585818390b7e88599a52056d5a7
Jose Carlos Joao Moreno Aleman <joao.moreno.aleman@gmail.com>
2023-11-07 11:44:02 -0500

    build (splash): Change - Creation of the application splashscreen

---
+++ app/src/main/java/com/systemfailed/rotomdex/ui/splashscreen/Splash.kt
@@ -0,0 +1,61 @@
+package com.systemfailed.rotomdex.ui.splashscreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.systemfailed.rotomdex.R
+import com.systemfailed.rotomdex.ui.navigation.AppScreens
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun SplashScreen(navController: NavController){
+
+    LaunchedEffect(key1 = true){
+        delay(5000)
+        navController.popBackStack()
+        navController.navigate(AppScreens.OnBoardingScreen.route)
+
+    }
+    Splash()
+
+}
+
+@Composable
+fun Splash() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+            .background(color = Color.DarkGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_pokedek),
+            contentDescription = "Logo",
+            Modifier.size(240.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    Splash()
+}
