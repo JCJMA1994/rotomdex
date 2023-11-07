eaa35accc3914be50a53a3ff23f0652bf13bcc33
Jose Carlos Joao Moreno Aleman <joao.moreno.aleman@gmail.com>
2023-11-07 11:46:07 -0500

    build (splash): Change - Application initialization

---
+++ app/src/main/java/com/systemfailed/rotomdex/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.systemfailed.rotomdex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.systemfailed.rotomdex.ui.navigation.AppNavigation
+import com.systemfailed.rotomdex.ui.splashscreen.Splash
+import com.systemfailed.rotomdex.ui.theme.RotomdexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            RotomdexTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    AppNavigation()
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    Splash()
+}
