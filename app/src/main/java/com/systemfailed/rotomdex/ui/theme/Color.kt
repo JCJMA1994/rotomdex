aa6b0b92e9803e7333b5ab339f0aeb6a963b1c07
Jose Carlos Joao Moreno Aleman <joao.moreno.aleman@gmail.com>
2023-11-09 11:41:50 -0500

    build (Color.kt): Change - Modified color

--- app/src/main/java/com/systemfailed/rotomdex/ui/theme/Color.kt
+++ app/src/main/java/com/systemfailed/rotomdex/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.systemfailed.rotomdex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Black = Color(0xFF000113)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -11,7 +13,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BlueGray = Color(0xFFA0A3BD)
-val WhiteGray = Color(0xFFB0B3B8)
+val DarkBlue = Color(0xFF000029)
 
 //Button Color
 val BlueColorButton = Color(0xFF173EA5)
